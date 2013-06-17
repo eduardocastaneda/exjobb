9169d2cf43789998e1b1ee5f54f593b46b9c0662
eduardocastaneda <eduardo.castaneda@valtech.se>
2013-06-17 21:35:49 +0200

    det aer dagboken och mobile-first

--- Implementation/mobile-first/main.js
+++ Implementation/mobile-first/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-		$('.menubutton').click(function(){
+	onClick();
+});
+
+$(window).resize(function() {
+    checkSize();
+});
+
+function checkSize() {
+    if ($(window).width() < 768) {
+        $('.main-nav').hide();
+    }
+    if($(window).width() >= 768){
+    	$('.main-nav').show();
+    }
+}
+
+function onClick() {
+	$('.menubutton').click(function(){
 			$('.main-nav').toggle();
 	});
-});
+}
