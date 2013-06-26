87ecf2562884d59c5629eb1c57e9b9250fc54861
eduardocastaneda <eduardo.castaneda@valtech.se>
2013-06-26 17:19:36 +0200

    efter en del kodgransknings har jag borjat andra allt efter feedback

--- Implementation/desktop-first/main.js
+++ Implementation/desktop-first/main.js
@@ -2,23 +2,5 @@ $(document).ready(function(){
 
 		$('.mobilemenu img').click(function(){
 			$('.main-nav').toggle();
-		});
-
-
-		$(window).resize(function() {   
-        if($(window).width() > 768)
-        {
-             $('.main-nav').show();
-        }
-        if($(window).width() <= 768)
-        {
-             $('.main-nav').hide();
-        }
-
-        if($(window).width() == 320)
-        {
-             console.log('mobiel breeee');
-        }
-    });
-	
+		});	
 });
--- Implementation/mobile-first/main.js
+++ Implementation/mobile-first/main.js
@@ -2,19 +2,6 @@ $(document).ready(function(){
 	onClick();
 });
 
-$(window).resize(function() {
-    checkSize();
-});
-
-function checkSize() {
-    if ($(window).width() < 768) {
-        $('.main-nav').hide();
-    }
-    if($(window).width() >= 768){
-    	$('.main-nav').show();
-    }
-}
-
 function onClick() {
 	$('.menubutton').click(function(){
 			$('.main-nav').toggle();
